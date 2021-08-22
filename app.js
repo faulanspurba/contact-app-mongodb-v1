d27a9815234987ab471e66c90b7fca108b3bd842
faulanspurba <faulans.purba2@gmail.com>
2021-08-22 21:33:22 +0700

    fixing some bugs

--- app.js
+++ app.js
@@ -1,35 +1,20 @@
-// EXPRESS
 const express = require("express");
 const app = express();
 const port = 3000;
 
-// DATABASE Connections
-require("./utils/db");
-
-// METHOD-OVERRIDE
 const methodOverride = require("method-override");
 app.use(methodOverride("_method"));
-// MODELS
-//contact
+
+require("./utils/db");
+
 const Contact = require("./models/contact");
 
-// EJS
-app.set("view engine", "ejs");
-
-// EJS Layouts
 const expressLayouts = require("express-ejs-layouts");
-app.use(expressLayouts);
-
-// req.body
-app.use(express.urlencoded({ extended: true }));
-
-// VALIDATOR
 const { body, validationResult, check } = require("express-validator");
-
-// FLASH MESSAGE
 const session = require("express-session");
 const cookieParser = require("cookie-parser");
 const flash = require("connect-flash");
+
 // Konfigurasi flash
 app.use(cookieParser("secret"));
 app.use(
@@ -44,42 +29,51 @@ app.use(
 );
 app.use(flash());
 
-// ***--ROUTER--***
+// Menngunakan ejs
+app.set("view engine", "ejs");
+// Third-party middleware
+app.use(expressLayouts);
 
-// Home
-app.get("/", (req, res) => {
+// Built-in middleware
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", async (req, res) => {
+  const contacts = await Contact.find();
   res.render("index", {
     title: "Home",
-    layout: "layouts/main-layout",
+    layout: "./layouts/main-layout",
+    contacts,
   });
 });
-// About
+
 app.get("/about", (req, res) => {
   res.render("about", {
     title: "About",
-    layout: "layouts/main-layout",
+    layout: "./layouts/main-layout",
   });
 });
-// Contact
+
 app.get("/contact", async (req, res) => {
   const contacts = await Contact.find();
+
   res.render("contact", {
     title: "Contact",
-    layout: "layouts/main-layout",
+    layout: "./layouts/main-layout",
     contacts,
     msg: req.flash("msg"),
   });
 });
 
-// Add-data routes
+// Add-data Contact Routes
 app.get("/contact/add", (req, res) => {
   res.render("add-contact", {
-    title: "Add contact",
-    layout: "layouts/main-layout",
+    title: "Add Contact",
+    layout: "./layouts/main-layout",
   });
 });
 
-// Add data contact
+// Add-data contact Process
 app.post(
   "/contact",
   [
@@ -111,31 +105,32 @@ app.post(
   }
 );
 
-// Delete data contact
+// Hapus data
 app.delete("/contact", async (req, res) => {
   const contact = await Contact.findOne({ _id: req.body._id });
   if (!contact) {
     res.status(404);
-    res.send(`<h1>Error(404) : File not found!</h1>`);
+    res.send(`<h1>404</h1>`);
   } else {
     Contact.deleteOne({ _id: contact._id }).then((result) => {
-      req.flash("msg", "Kontak berhasil dihapus");
+      // MESSAGE
+      req.flash("msg", "Kontak berhasil dihapus!");
       res.redirect("/contact");
     });
   }
 });
 
-// Routes edit data
+// Edit-contact Routes
 app.get("/contact/edit/:_id", async (req, res) => {
   const contact = await Contact.findOne({ _id: req.params._id });
   res.render("edit-contact", {
-    title: "Edit",
-    layout: "layouts/main-layout",
+    title: "Edit Contact",
+    layout: "./layouts/main-layout",
     contact,
   });
 });
 
-// Update Data
+// Process Edit Data
 app.put(
   "/contact",
   [
@@ -152,6 +147,7 @@ app.put(
   (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
+      // return res.status(400).json({ errors: errors.array() });
       res.render("edit-contact", {
         title: "Edit",
         layout: "./layouts/main-layout",
@@ -159,16 +155,7 @@ app.put(
         contact: req.body,
       });
     } else {
-      Contact.updateOne(
-        { _id: req.body._id },
-        {
-          $set: {
-            name: req.body.name,
-            email: req.body.email,
-            mPhone: req.body.mPhone,
-          },
-        }
-      ).then((result) => {
+      Contact.updateOne(req.body, (error, result) => {
         // Kirimkan flash message
         req.flash("msg", "Kontak berhasil diubah!");
         res.redirect("/contact");
@@ -177,7 +164,22 @@ app.put(
   }
 );
 
-// ***--ROUTER--***
+// Halaman detail kontak
+app.get("/contact/:_id", async (req, res) => {
+  const contact = await Contact.findOne({ _id: req.params._id });
+
+  res.render("details", {
+    title: "Detail Contact",
+    layout: "./layouts/main-layout",
+    contact,
+  });
+});
+
+app.use("/", (req, res) => {
+  res.status(404);
+  res.send(`Error : File not found`);
+});
+
 app.listen(port, () =>
-  console.log(`App listening at http://localhost:${port}`)
+  console.log(`App listening to http://localhost:${port}`)
 );
